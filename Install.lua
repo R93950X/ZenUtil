2295057acc28bc27e3fc7b50962e212ebc5d9b7f
R93950X <39017175+R93950X@users.noreply.github.com>
2020-12-05 16:00:53 -0800

    Ninja fix

--- Install.lua
+++ Install.lua
@@ -6,11 +6,33 @@ local Files = {
     "ZenPic.lua",
     "ZenText.lua",
 }
+
+toDownload = {}
+term.clear()
+term.setCursorPos(1,1)
+print("ZenUtil 0.02c Installer\n")
 for i, v in pairs(Files) do
-    print("obtaining https://raw.githubusercontent.com/R93950X/ZenUtil/main/"..v)
+    local input = 0
+    print("Do you wish to install "..v.."? [Y/N]")
+    repeat
+        input = select(2, os.pullEvent("key"))
+        
+    until input == keys.y or input == keys.n
+    if input == keys.y then
+        table.insert(toDownload, v)
+        
+    end
+    
+end
+
+for i, v in pairs(toDownload) do
+    write("Connecting to https://raw.githubusercontent.com/R93950X/ZenUtil/main/"..v.."... ")
     local website = http.get("https://raw.githubusercontent.com/R93950X/ZenUtil/main/"..v)
+    print("Success.")
     local file = fs.open("/ZenUtil/"..v,"w")
     file.write(website.readAll())
     website.close()
     file.close()
+    print("Downloaded as /ZenUtil/"..v)
+    
 end
