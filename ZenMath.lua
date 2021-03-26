514db141a773127d3326dbac8dba24fd12959d6a
R93950X <39017175+R93950X@users.noreply.github.com>
2021-03-26 14:14:59 -0700

    Fixed argument checking

--- ZenMath.lua
+++ ZenMath.lua
@@ -7,7 +7,7 @@ local function argumentCheck(...)
     local Args = {...}
     for i = 2, #Args, 2 do
         if type(Args[i-1]) ~= Args[i] then
-            error("Argument "..(i/2).." - expected type: "..Args[i+1]..", got "..type(Args[i]), 2)
+            error("Argument "..(i/2).." - expected type: "..Args[i]..", got "..type(Args[i-1]), 2)
 
         end
 
