becbf0a2b6f8d448eaa661ffd0989fc07017287b
R93950X <39017175+R93950X@users.noreply.github.com>
2021-07-05 00:42:14 -0700

    InstallDir functionality complete

--- .Install.lua
+++ .Install.lua
@@ -1,4 +1,8 @@
-local args = table.concat({...})
+local args = {...}
+for i = 1, #args do
+    args[i] = args[i].." "
+end
+args = table.concat(args)
 
 -- List of files for the user to select from
 local files = {
@@ -11,8 +15,10 @@ local files = {
 }
 
 local branch = string.find(args,"-b") and "beta" or "main"
-local installDir1, installDir2, installDir = args:find("-d [%w/]+")
-if installDir2 - installDir1 >= 4 then
+
+local installDir1, installDir2 = args:find("-d [%w/]+")
+local installDir
+if (installDir2 or 0) - (installDir1 or 0) >= 4 then
     installDir = "/"..args:sub(installDir1+3,installDir2)
 else
     installDir = "/ZenUtil"
@@ -25,7 +31,7 @@ term.setCursorPos(1,1)
 if branch == "main" then
     print("ZenUtil 0.03 Installer\n")
 else
-    print("ZenUtil 0.04 "..branch.. " Installer\n")
+    print("ZenUtil 0.04 "..branch.. " Installer\n@ "..installDir.."\n")
 end
 
 for i, v in pairs(files) do
@@ -47,11 +53,11 @@ for i, v in pairs(toDownload) do
     write("Connecting to https://raw.githubusercontent.com/R93950X/ZenUtil/"..branch.."/"..v.."... ")
     local website = http.get("https://raw.githubusercontent.com/R93950X/ZenUtil/"..branch.."/"..v)
     print("Success!")
-    local file = fs.open("/ZenUtil/"..v,"w")
+    local file = fs.open(installDir.."/"..v,"w")
     file.write(website.readAll():gsub("_BRANCH_",branch))
     website.close()
     file.close()
-    print("Downloaded as /ZenUtil/"..v)
+    print("Downloaded as "..installDir.."/"..v)
     
 end
 
@@ -61,5 +67,4 @@ Todo:
     - Somehow unify the argument check function in a modular utility pack
     - Make a UI
     - Create Update functionality
-    - Add installDir functionality
 ]]
